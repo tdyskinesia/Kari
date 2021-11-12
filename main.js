94e1ff7e5b1c320e9e7fafd578dc52d4df9899fb
Max Zukof <tdyskinesia@gmail.com>
2021-11-12 06:06:51 -0500

    prefix change

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-const prefix = '-';
+const prefix = '!k';
 
 const fs = require('fs');
 const { id } = require('date-fns/locale');
