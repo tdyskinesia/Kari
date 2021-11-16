a1e141f845cea7a93e95346daf7c717533205544
Max Zukof <tdyskinesia@gmail.com>
2021-11-16 05:24:39 -0500

    changed intents

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ const moment = require('moment-timezone');
 
 const Discord = require('discord.js'); 
 
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 
 const prefix = 'k!';
