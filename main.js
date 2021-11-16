37b1fc36286bdd50e9dbf64e57bf5eba87852d4c
Max Zukof <tdyskinesia@gmail.com>
2021-11-16 05:29:29 -0500

    flags

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const Discord = require('discord.js');
 
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS] });
 
 
 const prefix = 'k!';
