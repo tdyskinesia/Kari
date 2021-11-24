cdbf0728e75aa80e31ac10f2269eaf67ae407320
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 03:42:33 -0500

    fixed discord guild get error in message handler

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -5,12 +5,13 @@ const talentSchema = require('../data/talentSchema');
 const mongoose = require('mongoose');
 
 const iterate = async(client) => {
-    for await(const talent of talentSchema.talent.find(!{guildID: '838711689125822474'})){
+    let guild = await client.guilds.cache.get('835723287714857031')
+    for await(const talent of talentSchema.talent.find({guildID: '835723287714857031'})){
         talent.upcomingStreams.forEach(async function(stream){
             let curDate = new Date(stream.startTime)
             let curID = stream.videoId
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
-                await (await client.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${curID}`)
+                await (await guild.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${curID}`)
                 talentSchema.stream.deleteOne({id: stream.id}, function (err, id){
                     if(err) console.log(err)
                     console.log(`Deleted ${id}`)
