6cecdfaa5da0424acd75c9a791cfdab3bbbc9cd0
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 07:23:47 -0500

    changed subdocument deletion to $pull request

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -6,20 +6,25 @@ const mongoose = require('mongoose');
 
 const iterate = async(client) => {
     let guild = await client.guilds.cache.get('835723287714857031')
+    let arr = []
     for await(const talent of talentSchema.talent.find({guildID: '835723287714857031'})){
         talent.upcomingStreams.forEach(async function(stream){
             let curDate = new Date(stream.startTime) 
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
                 await (await guild.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${stream.videoID}`)
-                talentSchema.stream.findOneAndDelete({_id: stream.id}, function (err, id){
-                    if(err){ console.log(err)}
-                    else{
-                    console.log(`Deleted ${id.id}`)
-                    }
-                })
+                arr.push([talent.id, stream.id])
             }
         });
         await talent.save();
+   }
+   for(var i of arr){
+    talentSchema.talent.findOneAndUpdate({_id: arr[i][0]}, {
+        '$pull': {
+            'upcomingStreams':{ '_id': arr[i][1] } 
+        }, function (error, result){
+            console.log(result)
+        }
+    })
    }
 }
 
