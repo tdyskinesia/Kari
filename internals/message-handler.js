acdf0df18b7c7df38810ac6c5cfed9317095cb88
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 03:34:15 -0500

    trying other ways of iterating through notifications

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -6,13 +6,16 @@ const mongoose = require('mongoose');
 
 const iterate = async(client) => {
     for await(const talent of talentSchema.talent.find(!{guildID: '838711689125822474'})){
-        for await(const stream of talent.upcomingStreams){
+        talent.upcomingStreams.forEach(async function(stream){
             let curDate = new Date(stream.startTime)
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
                 await (await client.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${stream.videoID}`)
-                talent.upcomingStreams.splice(i, 1)
+                talentSchema.stream.deleteOne({id: stream.id}, function (err, id){
+                    if(err) console.log(err)
+                    console.log(`Deleted ${id}`)
+                })
             }
-        }
+        });
         await talent.save();
    }
 }
