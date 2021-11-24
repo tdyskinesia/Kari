d92a1cca5d50ba516756a953ddc0694f12dd3065
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 05:48:54 -0500

    removed maintenance arg

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ client.once('ready', async () =>{
         useNewUrlParser: true,
         keepAlive: true
     }).then(console.log("Connected to mongodb"));
-    statusChange(client, maintenance);
+    statusChange(client);
     var initialJob = new CronJob('0 */3 * * *', async function() {
         {
         /*console.log("I AM UPDATING STREAM TIMES NOW");
