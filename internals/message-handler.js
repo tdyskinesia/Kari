4086f325d00ef076831b289851730459eb866620
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 08:10:19 -0500

    changed pull syntax again

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -6,17 +6,20 @@ const mongoose = require('mongoose');
 
 const iterate = async(client) => {
     let guild = await client.guilds.cache.get('835723287714857031')
-    let arr = []
     for await(const talent of talentSchema.talent.find({guildID: '835723287714857031'})){
         talent.upcomingStreams.forEach(async function(stream){
             let curDate = new Date(stream.startTime) 
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
                 await (await guild.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${stream.videoID}`)
-                talentSchema.talent.findOneAndUpdate({_id: talent.id}, {
+                await talentSchema.talent.findOneAndUpdate({'_id': talent.id}, {
                     '$pull': {
-                        'upcomingStreams':{ _id: stream.id } 
+                        'upcomingStreams':{ '_id': stream.id } 
                     }, function (error, result){
+                        if(err){
+                            console.log(error)
+                        } else {
                         console.log(result)
+                        }
                     }
                 })
             }
