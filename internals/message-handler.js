07921f2e2dd43b54a485aed8551d9ade6f435430
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 03:22:24 -0500

    encapsulated stream iteration in message handler

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -9,7 +9,7 @@ const iterate = async(client) => {
         for await(const stream of talent.upcomingStreams){
             let curDate = new Date(stream.startTime)
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
-                await (await client.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${talent.upcomingStreams[i].videoID}`)
+                await (await client.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${stream.videoID}`)
                 talent.upcomingStreams.splice(i, 1)
             }
         }
