7f2416f9667272906810f1101775556903b5788d
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 03:18:14 -0500

    changed iteration to const stream instead of iterating by integer array index

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 
 const iterate = async(client) => {
     for await(const talent of talentSchema.talent.find(!{guildID: '838711689125822474'})){
-        for await(var i of talent.upcomingStreams){
-            let curDate = new Date(talent.upcomingStreams[i].startTime)
+        for await(const stream of talent.upcomingStreams){
+            let curDate = new Date(stream.startTime)
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
                 await (await client.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${talent.upcomingStreams[i].videoID}`)
                 talent.upcomingStreams.splice(i, 1)
