1c04a2608944ecdbc705730b3308c8ca58a49efe
Max Zukof <tdyskinesia@gmail.com>
2021-11-24 07:55:39 -0500

    nested with message because null array

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -12,21 +12,17 @@ const iterate = async(client) => {
             let curDate = new Date(stream.startTime) 
             if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
                 await (await guild.channels.cache.get(talent.liveChannelID)).send(`Hey ${talent.roleID}! ${talent.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${stream.videoID}`)
-                arr.push([talent.id, stream.id])
-                console.log(arr)
+
+                talentSchema.talent.findOneAndUpdate({_id: talent.id}, {
+                    $pull: {
+                        'talent.$.upcomingStreams':{ _id: stream.id } 
+                    }, function (error, result){
+                        console.log(result)
+                    }
+                }).save()
             }
         });
         await talent.save();
-   }
-   console.log(arr[0][0])
-   for await(var i of arr){
-    talentSchema.talent.findOneAndUpdate({_id: arr[i][0]}, {
-        $pull: {
-            'talent.$.upcomingStreams':{ _id: arr[i][1] } 
-        }, function (error, result){
-            console.log(result)
-        }
-    }).save()
    }
 }
 
