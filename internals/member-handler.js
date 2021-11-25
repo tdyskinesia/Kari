08a27699c61acddbc0da9c6a0f4290a7d26aad3c
Max Zukof <tdyskinesia@gmail.com>
2021-11-25 09:54:51 -0500

    added clone()

--- internals/member-handler.js
+++ internals/member-handler.js
@@ -11,7 +11,7 @@ const findTalentName = async(talentName, guildID) => {
         if(res){
         return res.name;
         }
-    })
+    }).clone()
 }
 
 const insertTalentMembership = async (guildID, talentName, inputMembership) => {
