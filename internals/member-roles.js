2da0627d75b6d63eb5bdac9e1e94621d46409aca
Max Zukof <tdyskinesia@gmail.com>
2021-11-25 07:48:55 -0500

    trying to cache channels first

--- internals/member-roles.js
+++ internals/member-roles.js
@@ -7,35 +7,16 @@ const mongoose = require('mongoose');
 const {inputMember} = require('./member-handler.js')
 
 module.exports = async(client) => {
-
+let arr = []
 for await(const channel of member_channel.find().lean()){
     let data = channel.verificationIDs
     for(var i in data){
-        await (await client.channels.cache.get(channel.channelID)).messages.fetch(data[i])
+        arr.push(await (await client.channels.cache.get(channel.channelID)).messages.fetch(data[i]))
         console.log(data[i])
     }
 }
+return arr;
 
 
-client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.message.partial) await reaction.message.fetch();
-    if (reaction.partial) await reaction.fetch();
-    if (user.partial) await user.fetch();
-    if (user.bot) return;
-    if (!reaction.message.guild) return;
-    console.log(reaction.message.id)
-    member_channel.findOne({guildID: reaction.message.guildId}, async(err, res)=>{
-        if(err) console.log(err)
-        if(res.channelID==reaction.message.channel.id){
-            if (reaction.emoji.name === '❌') {
-                await reaction.message.channel.send(`<@&${reaction.message.author.id}>, ${user.username} has marked your membership application as invalid. Please review and resubmit.`)
-            } else if (reaction.emoji.name === '✅'){
-                await reaction.message.channel.send(`<@&${reaction.message.author.id}>, ${user.username} has marked your membership as valid.`)
-                inputMember(await reaction.message.fetch(), reaction.message.author.id, user.id)
-
-            }
-        }
-    })
-})
 
 }
--- main.js
+++ main.js
@@ -47,6 +47,8 @@ const prefix = 'k!';
 const fs = require('fs');
 const { id } = require('date-fns/locale');
 
+var cache = []
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -65,7 +67,7 @@ client.once('ready', async () =>{
         useNewUrlParser: true,
         keepAlive: true
     }).then(console.log("Connected to mongodb"));
-    statusChange(client);
+    cache = statusChange(client);
 
     var initialJob = new CronJob('0 */3 * * *', async function() {
         {
@@ -113,6 +115,26 @@ client.once('ready', async () =>{
     
 });
 
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.partial) await user.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+    console.log(reaction.message.id)
+    member_channel.findOne({guildID: reaction.message.guildId}, async(err, res)=>{
+        if(err) console.log(err)
+        if(res.channelID==reaction.message.channel.id){
+            if (reaction.emoji.name === '❌') {
+                await reaction.message.channel.send(`<@&${reaction.message.author.id}>, ${user.username} has marked your membership application as invalid. Please review and resubmit.`)
+            } else if (reaction.emoji.name === '✅'){
+                await reaction.message.channel.send(`<@&${reaction.message.author.id}>, ${user.username} has marked your membership as valid.`)
+                inputMember(await reaction.message.fetch(), reaction.message.author.id, user.id)
+
+            }
+        }
+    })
+})
 // client.on('messageReactionAdd', async (reaction, user) => {
 //     if (reaction.message.partial) await reaction.message.fetch();
 //     if (reaction.partial) await reaction.fetch();
