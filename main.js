cd8421f029c669da301a91464e8582067c75a494
Max Zukof <tdyskinesia@gmail.com>
2021-11-28 15:54:24 -0500

    reimplemented migrate command

--- main.js
+++ main.js
@@ -119,9 +119,9 @@ client.on('message', message =>{
     else if(command === 'mtlist'){
         memberHandler.talentMembers(message, args)
     }
-    // else if(command === 'migrate'){
-    //     memberHandler.migrate2(message, args)
-    // }
+    else if(command === 'migrate'){
+        memberHandler.migrate2(message, args)
+    }
     else if(command === 'mtalentsetup'){
         memberHandler.subMembershipTalent(message, args)
     }
