d7f3f6ac91fc3369d598073b547171847238c046
Max Zukof <tdyskinesia@gmail.com>
2021-11-28 02:27:30 -0500

    date handling mutated inside if statement

--- internals/member-handler.js
+++ internals/member-handler.js
@@ -148,7 +148,7 @@ module.exports = {
         let d = new Date(args[1])
         let now = new Date()
         if(d!=null){
-        if(d.setDate(d.getDate()-32)<now&&d>now){
+        if(d.setDate(d.getDate()-32)<now&&d.setDate(d.getDate()+32)>now){
         let tal = await talent.findOne({guildID: message.guildId, name:{ $regex: '.*'+ args[0]+ '.*', $options: 'i' } }).lean().exec()
             if(tal!=null){
                 if(tal.memberRoleID){
