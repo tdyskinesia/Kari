bf19eaccb629f8353a273bfa91c80f9d420df69a
Max Zukof <tdyskinesia@gmail.com>
2021-11-29 12:23:29 -0500

    attempting to use proxies to get around blacklisting

--- main.js
+++ main.js
@@ -60,29 +60,6 @@ for(const file of commandFiles){
     client.commands.set(command.name, command);
 }
 
-const options = {
-    target: 'http://www.example.org', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-    pathRewrite: {
-      '^/api/old-path': '/api/new-path', // rewrite path
-      '^/api/remove/path': '/path', // remove base path
-    },
-    router: {
-      // when request.headers.host == 'dev.localhost:3000',
-      // override target 'http://www.example.org' to 'http://localhost:8000'
-      'dev.localhost:3000': 'http://localhost:8000',
-    },
-  };
-  // create the proxy (without context)
-const exampleProxy = createProxyMiddleware(options);
-
-// mount `exampleProxy` in web server
-const app = express();
-app.use('/api', exampleProxy);
-app.listen(3000);
-
-
 client.once('ready', async () =>{
     console.log('Online');
     await mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING || '', {
