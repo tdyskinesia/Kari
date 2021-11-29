c81bf4dc9b224fdb55ef1497df6c32b09f157f69
Max Zukof <tdyskinesia@gmail.com>
2021-11-29 10:28:06 -0500

    deleting cookies

--- internals/scrape.js
+++ internals/scrape.js
@@ -75,6 +75,7 @@ const getPage = async(driver, url)=>{
         // console.log(await t.getText())
         // let a = (await t.getText()).split('\n')[0]
         // if (a.includes("#")) a = (await t.getText()).split('\n')[1]
+        await driver.manage().deleteAllCookies()
         return await t.getText()
     } else {
         return null;
