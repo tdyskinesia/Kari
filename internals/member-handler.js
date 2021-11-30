058db91e15ae38f91637be5f4a1a61d6bc4e41ff
Max Zukof <tdyskinesia@gmail.com>
2021-11-29 20:24:41 -0500

    fixed user agent

--- internals/member-handler.js
+++ internals/member-handler.js
@@ -420,8 +420,7 @@ module.exports = {
             let guild = await client.guilds.fetch(mship.guildID)
             
             await DM.send(`Hi! You have one more day to renew your membership to ${mship.talentName}! Please verify in ${guild.name}'s verification channel. Thank you!`)
-            mship.notifyFlag = true                                                                                                                                //(guild.channels.fetch((await member_channel.findOne({guildID: guild.id})).channelID)).toString()
-            await mship.save();
+            await membership.findByIdAndUpdate(mship._id, {notifyFlag: false}).exec()
             return
         } catch (e) {
             console.log(e)
