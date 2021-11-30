30dee8b9caacce90ee76957023d89e78b5233a61
Max Zukof <tdyskinesia@gmail.com>
2021-11-29 20:54:26 -0500

    fixed user agent

--- internals/member-handler.js
+++ internals/member-handler.js
@@ -417,8 +417,8 @@ module.exports = {
         try{
             let userID = mship.userID
             let DM = await client.users.cache.get(userID).createDM()
-            await models.guild.findOne({membership_IDs: ObjectId(mship._id)})
-            let guild = await client.guilds.fetch((await models.guild.findOne({membership_IDs: ObjectId(mship._id)})).guildID)
+            let g = await models.guild.findOne({'membership_IDs': ObjectId(mship._id)}).exec()
+            let guild = await client.guilds.fetch(g.guildID)
             
             await DM.send(`Hi! You have one more day to renew your membership to ${mship.talentName}! Please verify in ${guild.name}'s verification channel. Thank you!`)
             await membership.findByIdAndUpdate(mship._id, {'$set': {"notifyFlag": false}}).exec()
