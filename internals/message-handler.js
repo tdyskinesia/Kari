1c52fe1ccb99a1f0205f437b9face1d61b699a23
Max Zukof <tdyskinesia@gmail.com>
2021-12-01 08:25:05 -0500

    declared guild explicitly in message handler iteration

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -16,7 +16,7 @@ try{
                 let curDate = new Date(str.startTime) 
                 let curGuild = await client.guilds.fetch(guild.guildID)
                 if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
-                    await (curGuild.channels.fetch(tal.liveChannelID)).send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
+                    await (await curGuild.channels.fetch(tal.liveChannelID)).send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
                         if(tal.liveChannelID!=null){
                             let ch = await curGuild.channels.fetch(tal.liveChannelID)
                             if(ch.name.includes('🔊')||ch.name.includes('🛑')){
