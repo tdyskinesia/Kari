a4e8b679d8e864e484b1bedbc7dff49fec29ad49
Max Zukof <tdyskinesia@gmail.com>
2021-12-01 08:17:23 -0500

    declared guild explicitly in message handler iteration

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -5,18 +5,21 @@ const {talent, stream, user, membership, member_channel} = require('../data/mode
 const models = require('../data/models');
 
 const mongoose = require('mongoose');
-
+/**
+ * @param  {Discord.Client} client
+ */
 const iterate = async(client) => {
 try{
     for await(const guild of models.guild.find({notificationsFlag: true})){
         for await(const tal of talent.find({guildID: guild.guildID})){
             for await (const str of stream.find({talent_id: tal._id})){
                 let curDate = new Date(str.startTime) 
+                let curGuild = client.guilds.fetch(guild.guildID)
                 if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
-                    await (await guild.channels.cache.get(tal.liveChannelID)).send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
+                    await (await curGuild.channels.cache.get(tal.liveChannelID)).send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
                     let tal = await talent.findById(str.talent_id)
                         if(tal.liveChannelID!=null){
-                            let ch = await (await client.guilds.fetch(tal.guildID)).channels.fetch(tal.liveChannelID)
+                            let ch = await curGuild.channels.fetch(tal.liveChannelID)
                             if(ch.name.includes('🔊')||ch.name.includes('🛑')){
                                 await ch.setName('🔔'.concat(ch.name.substring(1)))
                             }
