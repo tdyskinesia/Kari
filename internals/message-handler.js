5d68f234afc4dfa6446ad199bee3ea6bd60e7b3d
Max Zukof <tdyskinesia@gmail.com>
2021-12-01 21:16:58 -0500

    changed status counter to be more forgiving for lower amounts of streams

--- internals/message-handler.js
+++ internals/message-handler.js
@@ -17,7 +17,8 @@ try{
                 let curGuild = await client.guilds.fetch(guild.guildID)
                 let ch = await curGuild.channels.fetch(tal.liveChannelID)
                 if(curDate.setMinutes(curDate.getMinutes()-15) < new Date()){
-                    await ch.send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
+                if(tal.roleID!=null) {await ch.send(`Hey <@&${tal.roleID}>! ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)
+                } else {await ch.send(`Hey NULL ROLE ${tal.name} is streaming in 15 minutes! Feel free to join us at https://www.youtube.com/watch?v=${str.videoID}`)}
                         if(tal.liveChannelID!=null){
                             let ch = await curGuild.channels.fetch(tal.liveChannelID)
                             if(ch.name.includes('🔊')||ch.name.includes('🛑')){
